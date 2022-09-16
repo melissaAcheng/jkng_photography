64499d4cab9961504c65bd10d1c2d42cda6292bd
melissaAcheng <melcheng12@gmail.com>
2022-09-16 14:15:59 -0400

    added contact info

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,21 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import * as styles from "../styles/contact.module.css";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Contact = () => {
   return (
     <Layout pageTitle={"Contact"}>
-      <p>Contact me on Instagram</p>
+      <div className={styles.contact}>
+        <StaticImage src="../images/jeff_portrait_2.jpg" alt="Jeff Portrait" />
+        <div className={styles.text}>
+          <h1>Let's Connect!</h1>
+          <p>DM me on Instagram to book a photo session or to collaborate!</p>
+          <a href="https://www.instagram.com/jkng_photography/" target="_blank" rel="noopener noreferrer">
+            @jkng_photography
+          </a>
+        </div>
+      </div>
     </Layout>
   );
 };
