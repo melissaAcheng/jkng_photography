bb67a185946853878aa3ff970c4877d052d38f4a
melissaAcheng <melcheng12@gmail.com>
2022-09-17 11:48:14 -0400

    update

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,60 @@
 import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import * as styles from "../styles/gallery.module.css";
 
 const Gallery = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      one: allMdx(filter: { frontmatter: { row: { eq: "one" } } }, sort: { fields: frontmatter___title, order: DESC }) {
+        nodes {
+          frontmatter {
+            title
+            slug
+            row
+            featuredImageAlt
+            featuredImage {
+              childImageSharp {
+                gatsbyImageData(width: 750, transformOptions: { cropFocus: CENTER }, aspectRatio: 1)
+              }
+            }
+          }
+        }
+      }
+      two: allMdx(filter: { frontmatter: { row: { eq: "two" } } }, sort: { fields: frontmatter___title, order: DESC }) {
+        nodes {
+          frontmatter {
+            title
+            slug
+            row
+            featuredImageAlt
+            featuredImage {
+              childImageSharp {
+                gatsbyImageData(width: 750, transformOptions: { cropFocus: CENTER }, aspectRatio: 1)
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+  console.log(data);
   return (
-    <div>
-      <h1>Gallery</h1>
+    <div className={styles.wrapper}>
+      <div className={styles.one}>
+        {data.one.nodes.map((node) => (
+          <div key={node.frontmatter.title}>
+            <GatsbyImage image={getImage(node.frontmatter.featuredImage)} alt={node.frontmatter.featuredImageAlt} />
+          </div>
+        ))}
+      </div>
+      <div className={styles.two}>
+        {data.two.nodes.map((node) => (
+          <div key={node.frontmatter.title}>
+            <GatsbyImage image={getImage(node.frontmatter.featuredImage)} alt={node.frontmatter.featuredImageAlt} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
