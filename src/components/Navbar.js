9902139c06c725d339d4422c38eac440075157da
melissaAcheng <melcheng12@gmail.com>
2022-09-22 09:38:08 -0400

    added dropdown menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,8 +13,15 @@ const Navbar = () => {
           <li>
             <Link to="/about">About</Link>
           </li>
-          <li>
-            <Link to="/">Dropdown</Link>
+          <li className={styles.dropdown}>
+            <button className={styles.dropbtn}>Portfolio</button>
+            <div className={styles.dropcontent}>
+              <Link to="/portfolio/weddings">Weddings</Link>
+              <Link to="/portfolio/events">Events</Link>
+              <Link to="/portfolio/portraits">Portraits</Link>
+              <Link to="/portfolio/families">Families</Link>
+              <Link to="/portfolio/couples">Couples</Link>
+            </div>
           </li>
           <li>
             <Link to="/contact">Contact</Link>
