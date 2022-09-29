2f8b9a0e7c84f223124ae04bb7013c52a5e15c04
melissaAcheng <melcheng12@gmail.com>
2022-09-29 17:12:44 -0400

    added hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { useState } from "react";
 import * as styles from "../styles/navbar.module.css";
+import Hamburger from "hamburger-react";
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   return (
     <div>
-      <nav className={styles.nav}>
-        <ul>
+      <nav className={`${navbarOpen ? styles.showMenu : ""}`}>
+        <div className={styles.hamburger}>
+          <Hamburger direction="left" toggled={navbarOpen} toggle={setNavbarOpen} color="#818181" />
+        </div>
+        <ul className={styles.desktopNav}>
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -23,6 +29,33 @@ const Navbar = () => {
               <Link to="/portfolio/couples">Couples</Link>
             </div>
           </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <ul className={styles.mobileNav}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/portfolio/weddings">Weddings</Link>
+          </li>
+          <li>
+            <Link to="/portfolio/events">Events</Link>
+          </li>
+          <li>
+            <Link to="/portfolio/portraits">Portraits</Link>
+          </li>
+          <li>
+            <Link to="/portfolio/families">Families</Link>
+          </li>
+          <li>
+            <Link to="/portfolio/couples">Couples</Link>
+          </li>
+
           <li>
             <Link to="/contact">Contact</Link>
           </li>
